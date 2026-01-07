b678b51dd42eef54f66821bc6b6c56c375c9f0ec
ainzenBleach <samuel.barros@sptech.school>
2026-01-07 16:22:42 -0300

    feat(fundamentals): Adding fundamentals folder with my first component

---
+++ fundamentals/components/FirtsComponents.jsx
@@ -0,0 +1,14 @@
+const FistComponents = () => {
+
+let soma = 1 + 2;
+
+    return(
+        <div>
+            <h1>My first component</h1>
+            <p>{soma}</p>
+        </div>
+    )
+
+}
+
+export default FistComponents;
---
+++ fundamentals/src/App.jsx
@@ -0,0 +1,19 @@
+//Components
+import FistComponents from '../components/FirtsComponents'
+
+//Style/css
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>React fundaments</h1>
+        <FistComponents/>
+      </div>
+    </>
+  )
+}
+
+export default App
