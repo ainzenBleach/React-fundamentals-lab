7cca6876519fc997317eb3bde1329c671c6ca43e
ainzenBleach <samuel.barros@sptech.school>
2026-01-10 15:08:30 -0300

    feat(Images): adding imagens between two paths: public folder and assents folder

--- advancing-React/src/App.jsx
+++ advancing-React/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// Components
+
+//style
 import './App.css'
+import Pokemon from "./assets/pokemon2.png"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>advancing in React</h1>
+
+      <h3>Imagem in public folder</h3>
+      <img src="/pokemon1.png" alt=""/>
+
+      <h3>Imagem in assents folder</h3>
+      <img src={Pokemon} alt="" />
+    </div>
   )
 }
 
