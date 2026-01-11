ce0efeb790a3e4679ee4901de6ad1d020a42e5a8
ainzenBleach <samuel.barros@sptech.school>
2026-01-11 10:56:12 -0300

    feat(Previus state): manipulation of list with previus state

--- advancing-React/src/components/ListRender.jsx
+++ advancing-React/src/components/ListRender.jsx
@@ -3,6 +3,25 @@ import { useState } from "react"
 const ListRender = () => {
 
     const [list] = useState(["John","Mark","Fred"])
+    
+    const [users, setUsers] = useState(
+        [
+            {id: 1, name: "Shag", age:"21"},
+            {id: 2, name: "Fred", age:"23"},
+            {id: 3, name: "Scoob", age:"24"}
+        ]    
+    )
+
+    const DeleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setUsers((prevUsers) => {
+
+            return prevUsers.filter((user) => randomNumber !== user.id)
+            
+        })
+    }
+
     return (
     <div>
         <ul>
@@ -10,6 +29,17 @@ const ListRender = () => {
                 <li key={i}>{item}</li>
             ))}
         </ul>
+
+        {/* Using ids coming a data base */}
+
+        <ul>
+            {users.map((users) => (
+                <li key={users.id}>{users.name} - {users.age}</li>
+            ))}
+        </ul>
+
+            <button onClick={DeleteRandom}> Detele random user</button>
+
     </div>
   )
 }
